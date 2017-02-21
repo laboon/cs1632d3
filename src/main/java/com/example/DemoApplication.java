bb563ab4e880cb989555028cb97d4173655d314c
laboon <laboon@gmail.com>
2017-02-21 11:29:14 -0500

    model

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -12,11 +12,11 @@ import org.springframework.stereotype.*;
 @SpringBootApplication
 public class DemoApplication {
 
-    @RequestMapping("/")
-    @ResponseBody
-    String home() {
-      return "Hello World!";
-    }
+    // @RequestMapping("/")
+    // @ResponseBody
+    // String home() {
+    //   return "Hello World!";
+    // }
 
     @RequestMapping("/bill")
     @ResponseBody
@@ -24,6 +24,14 @@ public class DemoApplication {
       return "Hello Bill!";
     }
 
+    @GetMapping("/")
+    public String home(Map<String, Object> model) {
+	model.put("message", "Hello World");
+	model.put("title", "Hello Home");
+	model.put("date", new Date());
+	return "home";
+    }
+    
     
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
